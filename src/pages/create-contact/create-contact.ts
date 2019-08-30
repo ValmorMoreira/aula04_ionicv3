4de224626638d76a8699d6f3fe00588af05f0667
ValmorMoreira <valfreestep@gmail.com>
2019-08-29 21:49:30 -0300

    Versão com atributos

--- src/pages/create-contact/create-contact.ts
+++ src/pages/create-contact/create-contact.ts
@@ -21,23 +21,29 @@ export class CreateContactPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private toast: ToastController, public contactsProvider: ContactsProvider
     ) {
     this.model = new Contact();
-    this.model.name = 'Novo contato';
+    this.model.name = 'Digite seu nome';
     this.model.gender = 'male';
+    this.model.birthday = '00/00/0000';
+    this.model.employed = true;
+    this.model.salary = 0;
   }
 
     createContact() {
     var data = {
       'contact': {
         'name': this.model.name,
-        'gender': this.model.gender
+        'gender': this.model.gender,
+        'birthday': this.model.birthday,
+        'employed': this.model.employed,
+        'salary': this.model.salary
       }
     };
     this.contactsProvider.addContact(data)
       .then((result: any) => {
-        this.toast.create({ message: 'Contato criado'}).present();
+        this.toast.create({ message: 'Contato criado', duration:3000}).present();
       })
       .catch((error: any) => {
-        this.toast.create({ message: 'Falha ao criar o contato: ' + JSON.stringify(error.error) }).present();
+        this.toast.create({ message: 'Falha ao criar o contato: ' + JSON.stringify(error.error),duration:3000 }).present();
         console.log(error);
       });
   }  
@@ -51,5 +57,8 @@ export class CreateContactPage {
 export class Contact {
   name: string;
   gender: string;
+  birthday: string;
+  employed: boolean;
+  salary: number;
 }
 
